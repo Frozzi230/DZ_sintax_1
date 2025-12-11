803e56dc533989b9e34df814d636e137cc3b6342
Frozzi230 <aaserik28011@gmai.com>
2025-12-11 22:00:54 +0400

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog + "\n" + cat + "\n" + paper);
+        System.out.println(dog + "\n\r" + cat + "\n\r" + paper);
 
         System.out.println( );
 
@@ -15,7 +15,7 @@ public class Main
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
-        System.out.println(dog + "\n" + cat + "\n" + paper);
+        System.out.println(dog + "\n\r" + cat + "\n\r" + paper);
 
         System.out.println( );
 
@@ -23,22 +23,28 @@ public class Main
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
-        System.out.println(dog + "\n" + cat + "\n" + paper);
+        System.out.println(dog + "\n\r" + cat + "\n\r" + paper);
 
         System.out.println( );
 
         System.out.println("Задача №4");
         var friend = 19;
-        System.out.println(friend + "\n" + (friend - 2) + "\n" + (friend - 2)/7);
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
         System.out.println( );
 
         System.out.println("Задача №5");
         var frog = 3.5;
-        var frog1 = frog * 10;
-        var frog2 = frog1 / 3.5;
-        var frog3 = frog2 + 4;
-        System.out.println(frog1 + "\n" + frog2 + "\n" + frog3);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
         System.out.println( );
 
